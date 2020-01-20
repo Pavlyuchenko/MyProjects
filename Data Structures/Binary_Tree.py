21fc59f7eeb491529078cc5166f147234acfc564
Pavlyuchenko <mmichal.pavlicek@seznam.cz>
2020-01-20 18:33:54 +0100

    Updates Tree.py

---
+++ Data Structures/Binary_Tree.py
@@ -0,0 +1,96 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+    
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, t_type):
+        if t_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        elif t_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif t_type == "postorder":
+            return self.postorder_print(tree.root, "")
+        elif t_type == "levelorder":
+            return self.level_order_print(tree.root)
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += str(start.value) + " -> "
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+    def inorder_print(self, start, traversal):
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += str(start.value) + " -> "
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+
+    def postorder_print(self, start, traversal):
+        if start:
+            traversal = self.postorder_print(start.left, traversal)
+            traversal = self.postorder_print(start.right, traversal)
+            traversal += str(start.value) + " -> "
+        return traversal
+
+    def level_order_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+
+        while len(queue) > 0:
+            node = queue.dequeue()
+
+            traversal += str(node.value) + " -> "
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
+                
+
+        
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.left.right.left = Node(6)
+tree.root.left.right.right = Node(7)
+
+print(tree.print_tree("levelorder"))
