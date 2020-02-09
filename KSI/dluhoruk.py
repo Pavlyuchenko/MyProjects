3de971757e38fc665543093910617583a220ce50
Pavlyuchenko <mmichal.pavlicek@seznam.cz>
2020-02-09 13:21:57 +0100

    Finished dlouhoruk.py

--- KSI/dluhoruk.py
+++ KSI/dluhoruk.py
@@ -11,17 +11,18 @@ class Tree:
 
 
 def check_dluhoruk(tree):
-    return check_dluhoruk_node(tree.root)
+    res = check_dluhoruk_node(tree.root, 1)
+    return res[2] == res[0]
 
-def check_dluhoruk_node(node):
+def check_dluhoruk_node(node, lower=0):
     if node.left and node.right:
         left = check_dluhoruk_node(node.left)
         right = check_dluhoruk_node(node.right)
 
         if 1 + left[1] + right[1] > max(left[0], right[0]):
-            longest = 1 + left[1] + right[1]
+            longest = 1 + left[1] + right[1] - lower
         else:
-            longest = max(left[0], right[0])
+            longest = max(left[0], right[0]) - lower
         optional = 1 + max(left[1], right[1])
         current = left[1] + right[1]
 
@@ -34,7 +35,7 @@ def check_dluhoruk_node(node):
             longest = 1 + left[1]
         else:
             longest = left[0]
-        optional = longest
+        optional = 1 + left[1]
 
         return [longest, optional]
     elif node.right:
@@ -44,7 +45,7 @@ def check_dluhoruk_node(node):
             longest = 1 + right[1]
         else:
             longest = right[0]
-        optional = longest
+        optional = 1 + right[1]
 
         return [longest, optional]
     return [1, 1]
@@ -75,7 +76,10 @@ t.root.right = Node(8)
 t.root.left.left = Node(2)
 t.root.left.right = Node(5)
 t.root.left.left.left = Node(3)
-t.root.left.right.right = Node(4)
+t.root.left.right.right = Node(6)
+t.root.left.left.left.left = Node(4)
+t.root.left.right.right.right = Node(7)
+
 
 t1 = Tree()
 t1.root = Node(0)
@@ -87,4 +91,30 @@ t1.root.left.right = Node(5)
 t1.root.left.left.left = Node(3)
 t1.root.left.left.right = Node(4)
 
-print(check_dluhoruk(t1))
+t2 = Tree()
+t2.root = Node(1)
+t2.root.left = Node(2)
+t2.root.right = Node(3)
+t2.root.right.left = Node(4)
+t2.root.right.right = Node(5)
+t2.root.right.left.left = Node(6)
+t2.root.right.right.right = Node(7)
+t2.root.right.left.left.left = Node(8)
+t2.root.right.right.right.right = Node(9)
+
+t3 = Tree()
+t3.root = Node(1)
+t3.root.left = Node(2)
+t3.root.left.left = Node(3)
+t3.root.left.left.left = Node(4)
+t3.root.left.left.left.left = Node(5)
+t3.root.left.left.left.left.left = Node(6)
+t3.root.left.left.left.left.left.left = Node(7)
+t3.root.left.left.right = Node(6)
+t3.root.left.left.right.right = Node(7)
+t3.root.left.left.right.right.right = Node(8)
+t3.root.left.left.right.right.right.right = Node(9)
+t3.root.right = Node(8)
+
+
+print(check_dluhoruk(t2))
